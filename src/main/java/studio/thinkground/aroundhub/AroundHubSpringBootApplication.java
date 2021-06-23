d7d8c6d2c2a7ba48afc46b7c9f2656969accfa2a
Flature <jungwoo5394@gmail.com>
2021-06-23 16:30:54 +0900

    Initial Commit

---
+++ src/main/java/studio/thinkground/aroundhub/AroundHubSpringBootApplication.java
@@ -0,0 +1,13 @@
+package studio.thinkground.aroundhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AroundHubSpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AroundHubSpringBootApplication.class, args);
+  }
+
+}
---
+++ src/test/java/studio/thinkground/aroundhub/AroundHubSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package studio.thinkground.aroundhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AroundHubSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
