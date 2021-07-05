ad97cd919368bc3bce04b4d82fd95a49ecd277c6
Flature <jungwoo5394@gmail.com>
2021-07-05 14:52:47 +0900

    [update] get api 관련 예시 업데이트

---
+++ src/main/java/studio/thinkground/aroundhub/dto/MemberDTO.java
@@ -0,0 +1,41 @@
+package studio.thinkground.aroundhub.dto;
+
+public class MemberDTO {
+
+  private String name;
+  private String email;
+  private String organization;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getOrganization() {
+    return organization;
+  }
+
+  public void setOrganization(String organization) {
+    this.organization = organization;
+  }
+
+  @Override
+  public String toString() {
+    return "MemberDTO{" +
+        "name='" + name + '\'' +
+        ", email='" + email + '\'' +
+        ", organization='" + organization + '\'' +
+        '}';
+  }
+}
