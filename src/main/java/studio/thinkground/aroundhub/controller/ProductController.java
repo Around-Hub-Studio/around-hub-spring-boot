a4ebe14ca0744205b193fae72b01172e917a2a8a
Flature <jungwoo5394@gmail.com>
2021-08-21 00:11:58 +0900

    [update] Spring Boot Validation 코드 예제 추가 (Hibernate-Validator)

--- src/main/java/studio/thinkground/aroundhub/controller/ProductController.java
+++ src/main/java/studio/thinkground/aroundhub/controller/ProductController.java
@@ -1,8 +1,11 @@
 package studio.thinkground.aroundhub.controller;
 
+import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import studio.thinkground.aroundhub.data.dto.ProductDto;
-import studio.thinkground.aroundhub.dto.MemberDTO;
 import studio.thinkground.aroundhub.service.ProductService;
 
 @RestController
@@ -22,7 +24,7 @@ public class ProductController {
   private ProductService productService;
 
   @Autowired
-  public ProductController(ProductService productService){
+  public ProductController(ProductService productService) {
     this.productService = productService;
   }
 
@@ -30,26 +32,42 @@ public class ProductController {
   @GetMapping(value = "/product/{productId}")
   public ProductDto getProduct(@PathVariable String productId) {
 
-    long startTime =System.currentTimeMillis();
+    long startTime = System.currentTimeMillis();
     LOGGER.info("[ProductController] perform {} of Around Hub API.", "getProduct");
 
-    ProductDto productDto =productService.getProduct(productId);
+    ProductDto productDto = productService.getProduct(productId);
 
-    LOGGER.info("[ProductController] Response :: productId = {}, productName = {}, productPrice = {}, productStock = {}, Response Time = {}ms", productDto.getProductId(),
-        productDto.getProductName(), productDto.getProductPrice(), productDto.getProductStock(), (System.currentTimeMillis()- startTime));
+    LOGGER.info(
+        "[ProductController] Response :: productId = {}, productName = {}, productPrice = {}, productStock = {}, Response Time = {}ms",
+        productDto.getProductId(),
+        productDto.getProductName(), productDto.getProductPrice(), productDto.getProductStock(),
+        (System.currentTimeMillis() - startTime));
     return productDto;
   }
 
   // http://localhost:8080/api/v1/product-api/product
   @PostMapping(value = "/product")
-  public ProductDto createProduct(@RequestBody ProductDto productDto) {
+  public ResponseEntity<ProductDto> createProduct(@Valid @RequestBody ProductDto productDto) {
+
+    // Validation Code Example
+    if (productDto.getProductId().equals("") || productDto.getProductId().isEmpty()) {
+      LOGGER.error("[createProduct] failed Response :: productId is Empty");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(productDto);
+    }
 
     String productId = productDto.getProductId();
     String productName = productDto.getProductName();
     int productPrice = productDto.getProductPrice();
     int productStock = productDto.getProductStock();
 
-    return productService.saveProduct(productId,productName,productPrice,productStock);
+    ProductDto response = productService
+        .saveProduct(productId, productName, productPrice, productStock);
+
+    LOGGER.info(
+        "[createProduct] Response >> productId : {}, productName : {}, productPrice : {}, productStock : {}",
+        response.getProductId(), response.getProductName(), response.getProductPrice(),
+        response.getProductStock());
+    return ResponseEntity.status(HttpStatus.OK).body(response);
   }
 
   // http://localhost:8080/api/v1/product-api/product/{productId}
--- src/main/java/studio/thinkground/aroundhub/data/dto/ProductDto.java
+++ src/main/java/studio/thinkground/aroundhub/data/dto/ProductDto.java
@@ -1,5 +1,9 @@
 package studio.thinkground.aroundhub.data.dto;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,9 +18,21 @@ import studio.thinkground.aroundhub.data.entity.ProductEntity;
 @Builder
 public class ProductDto {
 
+  //@Size(min = 8, max = 8) // abcdefg
+  @NotNull
   private String productId;
+
+  @NotNull
   private String productName;
+
+  @NotNull
+  @Min(value = 500)
+  @Max(value = 3000000)
   private int productPrice;
+
+  @NotNull
+  @Min(value = 0)
+  @Max(value = 9999)
   private int productStock;
 
   public ProductEntity toEntity(){
