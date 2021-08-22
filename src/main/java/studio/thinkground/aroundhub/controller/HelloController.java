db03b50b882b47659742474027196b6ec7ccd479
Flature <jungwoo5394@gmail.com>
2021-08-22 22:58:56 +0900

    [update] Spring Boot 예외 처리 방법 업데이트 1

--- src/main/java/studio/thinkground/aroundhub/controller/HelloController.java
+++ src/main/java/studio/thinkground/aroundhub/controller/HelloController.java
@@ -1,5 +1,11 @@
 package studio.thinkground.aroundhub.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +31,27 @@ public class HelloController {
     LOGGER.info("Info Log");
     LOGGER.warn("Warn Log");
     LOGGER.error("Error Log");
+  }
+
+  @PostMapping("/exception")
+  public void exceptionTest() throws Exception {
+    throw new Exception();
+  }
+
+  @ExceptionHandler(value = Exception.class)
+  public ResponseEntity<Map<String, String>> ExceptionHandler(Exception e) {
+    HttpHeaders responseHeaders = new HttpHeaders();
+    //responseHeaders.add(HttpHeaders.CONTENT_TYPE, "application/json");
+    HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+    LOGGER.info("Controller 내 ExceptionHandler 호출");
+
+    Map<String, String> map = new HashMap<>();
+    map.put("error type", httpStatus.getReasonPhrase());
+    map.put("code", "400");
+    map.put("message", "에러 발생");
+
+    return new ResponseEntity<>(map, responseHeaders, httpStatus);
   }
 
 }
---
+++ src/main/java/studio/thinkground/aroundhub/exception/AroundHubExceptionHandler.java
@@ -0,0 +1,35 @@
+package studio.thinkground.aroundhub.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import studio.thinkground.aroundhub.controller.ProductController;
+
+@RestControllerAdvice
+public class AroundHubExceptionHandler {
+
+  private final Logger LOGGER = LoggerFactory.getLogger(AroundHubExceptionHandler.class);
+
+  @ExceptionHandler(value = Exception.class)
+  public ResponseEntity<Map<String, String>> ExceptionHandler(Exception e) {
+    HttpHeaders responseHeaders = new HttpHeaders();
+    //responseHeaders.add(HttpHeaders.CONTENT_TYPE, "application/json");
+    HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+    LOGGER.info("Advice 내 ExceptionHandler 호출");
+
+    Map<String, String> map = new HashMap<>();
+    map.put("error type", httpStatus.getReasonPhrase());
+    map.put("code", "400");
+    map.put("message", "에러 발생");
+
+    return new ResponseEntity<>(map, responseHeaders, httpStatus);
+  }
+
+}
