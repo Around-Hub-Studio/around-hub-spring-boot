5d7faee780ed259a31ec3fb5acd46f03ec9ac68b
Flature <jungwoo5394@gmail.com>
2021-08-23 09:54:05 +0900

    [update] Spring Boot 예외 처리 방법 업데이트 1

--- src/main/java/studio/thinkground/aroundhub/controller/HelloController.java
+++ src/main/java/studio/thinkground/aroundhub/controller/HelloController.java
@@ -2,16 +2,15 @@ package studio.thinkground.aroundhub.controller;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @RestController
 public class HelloController {
@@ -44,6 +43,7 @@ public class HelloController {
     //responseHeaders.add(HttpHeaders.CONTENT_TYPE, "application/json");
     HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
 
+    LOGGER.info(e.getMessage());
     LOGGER.info("Controller 내 ExceptionHandler 호출");
 
     Map<String, String> map = new HashMap<>();
--- src/main/java/studio/thinkground/aroundhub/exception/AroundHubExceptionHandler.java
+++ src/main/java/studio/thinkground/aroundhub/exception/AroundHubExceptionHandler.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import studio.thinkground.aroundhub.controller.ProductController;
 
 @RestControllerAdvice
 public class AroundHubExceptionHandler {
