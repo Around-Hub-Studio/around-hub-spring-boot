d7315f7a9ff3c491a62cfb56c2a2dfb25e78db22
Flature <jungwoo5394@gmail.com>
2021-10-07 19:51:26 +0900

    [update] 테스트 커버리지 관련 업데이트

--- src/test/java/studio/thinkground/aroundhub/service/impl/ProductServiceImplTest.java
+++ src/test/java/studio/thinkground/aroundhub/service/impl/ProductServiceImplTest.java
@@ -4,15 +4,19 @@ import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import studio.thinkground.aroundhub.data.dto.ProductDto;
 import studio.thinkground.aroundhub.data.entity.ProductEntity;
 import studio.thinkground.aroundhub.data.handler.impl.ProductDataHandlerImpl;
 
-@SpringBootTest(classes = {ProductDataHandlerImpl.class, ProductServiceImpl.class})
+//@SpringBootTest(classes = {ProductDataHandlerImpl.class, ProductServiceImpl.class})
+@ExtendWith(SpringExtension.class)
+@Import({ProductDataHandlerImpl.class, ProductServiceImpl.class})
 public class ProductServiceImplTest {
 
   @MockBean
@@ -30,6 +34,9 @@ public class ProductServiceImplTest {
     ProductDto productDto = productService.getProduct("123");
 
     Assertions.assertEquals(productDto.getProductId(), "123");
+    Assertions.assertEquals(productDto.getProductName(), "pen");
+    Assertions.assertEquals(productDto.getProductPrice(), 2000);
+    Assertions.assertEquals(productDto.getProductStock(), 3000);
 
     verify(productDataHandler).getProductEntity("123");
   }
@@ -46,6 +53,8 @@ public class ProductServiceImplTest {
     Assertions.assertEquals(productDto.getProductName(), "pen");
     Assertions.assertEquals(productDto.getProductPrice(), 2000);
     Assertions.assertEquals(productDto.getProductStock(), 3000);
+
+    verify(productDataHandler).saveProductEntity("123", "pen", 2000, 3000);
   }
 
 
