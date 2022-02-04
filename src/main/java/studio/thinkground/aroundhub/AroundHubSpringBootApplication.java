901324a9b9fa145ac95de2259ca0df76c8ea5b98
Flature <jungwoo5394@gmail.com>
2022-02-04 11:37:01 +0900

    [update] Jasypt 적용

--- src/main/java/studio/thinkground/aroundhub/AroundHubSpringBootApplication.java
+++ src/main/java/studio/thinkground/aroundhub/AroundHubSpringBootApplication.java
@@ -1,11 +1,9 @@
 package studio.thinkground.aroundhub;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEncryptableProperties
 public class AroundHubSpringBootApplication {
 
     public static void main(String[] args) {
